6c33d476c9187bf1dd2af44d120d01d47085234a
CailieAC <cailiecarlile@gmail.com>
2019-05-19 18:46:23 -0500

    ?

--- CoderGirl_MVCMovies/Controllers/DirectorController.cs
+++ CoderGirl_MVCMovies/Controllers/DirectorController.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoderGirl_MVCMovies.Data;
+using CoderGirl_MVCMovies.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoderGirl_MVCMovies.Controllers
 {
+    public static IDirectorRepository directorRepository = RepositoryFactory.GetDirectorRepository();
+
     public class DirectorController : Controller
     {
         public IActionResult Index()
         {
-            return View();
+            List<Director> directors = DirectorRepository.GetDirectors();
+            return View(directors);
         }
 
         public IActionResult Create()
