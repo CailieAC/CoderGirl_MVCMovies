285bd4c6095f9ac4dbbfde309e4869c23a84d6af
CailieAC <cailiecarlile@gmail.com>
2019-06-04 23:34:27 -0500

    Start MovieListItemViewModel for Movie Index Page

---
+++ CoderGirl_MVCMovies/ViewModels/Movies/MovieListItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoderGirl_MVCMovies.ViewModels.Movies
+{
+    public class MovieListItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string DirectorName { get; set; }
+        public int Year { get; set; }
+        public List<int> Ratings { get; set; }
+    }
+}
